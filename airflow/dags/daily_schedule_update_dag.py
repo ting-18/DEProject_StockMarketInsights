e25a045ba94a21699ff4e1c0145e796843c41c77
ting-18 <momotina18@outlook.com>
2025-04-01 23:54:05 -0700

    add dbt run into airflow orchestration

--- airflow/dags/daily_schedule_update_dag.py
+++ airflow/dags/daily_schedule_update_dag.py
@@ -3,19 +3,23 @@ import logging
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from airflow.utils.task_group import TaskGroup
+# from airflow.utils.task_group import TaskGroup
+from airflow.models import Variable
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 # from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 from google.cloud import storage
+from google.cloud import bigquery
 # from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-
+# import pyarrow.csv as pv
+# import pyarrow.parquet as pq
 import pandas as pd
 # from datetime import date
-from dateutil.relativedelta import relativedelta
+# from dateutil.relativedelta import relativedelta
 import yfinance as yf
+
 
 #TODO：Replace with your own PROJECT_ID,BUCKET, DATESET
 PROJECT_ID = 'sylvan-earth-454218-d0'
@@ -28,23 +32,32 @@ TICKERS = pd.read_html(SP500_URL)[0]['Symbol'].tolist()
 TICKERS.append("^GSPC")  # append S&P 500 index tikcer
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
-def extract_data(tickers, n):
-    # Extrat daily stock data for the last 10 years each ticker from Yahoo Finance API
-    if n<10:
-        m = (n+1)*50
-    else:
-        m = len(tickers)
-    # today = date.today()
-    # ten_years_ago = today - relativedelta(years=1)  # It's scalable, set 1 for test here.
-    data = []
-    for ticker in tickers[n*50:m]:
+
+
+def get_last_processed_date():
+    """Retrieve the last processed date from Airflow Variables."""
+    return Variable.get("last_date", default_var="2025-03-25")
+
+def update_last_processed_date(new_date):
+    """Update the last processed date in Airflow Variables."""
+    Variable.set("last_date", new_date)
+
+last_date = get_last_processed_date()
+
+def extract_new_data(tickers, last_date):
+    # Extrat new daily stock data for each ticker from Yahoo Finance API
+    # last_date = get_last_processed_date()
+    new_data = []
+    for ticker in tickers:
         if '.' in ticker:
             ticker = ticker.replace(".", "-")
-        stock_data = yf.download(ticker, start="2024-01-01", end="2025-03-25", interval="1d")  # Customize as per your need
-        data.append(stock_data)
-
+        stock_data = yf.download(ticker, start=last_date, interval="1d")  # Customize as per your need
+        if stock_data.empty:
+            print("No new data to extract.")
+            return None
+        new_data.append(stock_data)
     pdf1 = pd.DataFrame()
-    for pdf in data:
+    for pdf in new_data:
         pdf['Ticker'] = pdf.columns[0][1]
         pdf.columns = pdf.columns.droplevel('Ticker')
         pdf.columns.name =None
@@ -53,7 +66,6 @@ def extract_data(tickers, n):
         pdf["Date"] = pdf["Date"].dt.date
         # # Or convert datetime64 to string
         # df["Date"] = pdf["Date"].astype(str)
-
         # As some parquet will store 'Volume' as int64, some as float
         # Check if 'Volume' is stored as float and Round,convert to int
         if pdf["Volume"].dtype == "float64":            
@@ -61,7 +73,10 @@ def extract_data(tickers, n):
         pdf=pdf.round(4)
         pdf2 =pdf.loc[:, ["Date","Ticker", "Open","Close","High","Low","Volume"]]
         pdf1= pd.concat([pdf1, pdf2])
-    pdf1.to_parquet(f"{path_to_local_home}/sp500_stocks_{n}.parquet", index=False)
+    pdf1.to_parquet(f"{path_to_local_home}/new_data.parquet", index=False)
+    if pdf1 is not None:
+        new_last_date = pdf1["Date"].max()
+        update_last_processed_date(new_last_date)
 
 
 def upload_to_gcs(bucket, object_name, local_file):
@@ -87,9 +102,36 @@ def upload_to_gcs(bucket, object_name, local_file):
     blob.upload_from_filename(local_file)
 
 
+def merge_into_bigquery(dataset_id, target_table, staging_table):
+    """Merge data from staging table into the main BigQuery table (UPSERT)."""
+    client = bigquery.Client()
+    query = f"""
+    MERGE `{dataset_id}.{target_table}` AS target
+    USING `{dataset_id}.{staging_table}` AS source
+    ON target.Ticker = source.Ticker AND target.Date = source.Date
+    WHEN MATCHED THEN 
+        UPDATE SET 
+            target.Open = source.Open,
+            target.High = source.High,
+            target.Low = source.Low,
+            target.Close = source.Close,
+            target.Volume = source.Volume
+    WHEN NOT MATCHED THEN 
+        INSERT (Date, Ticker, Open, High, Low, Close, Volume)
+        VALUES (source.Date, source.Ticker, source.Open, source.High, source.Low, source.Close, source.Volume);
+    """
+
+    query_job = client.query(query)
+    query_job.result()  # Wait for the job to complete
+
+    print(f"Merged staging table into {dataset_id}.{target_table}")
+
+
+
+
 default_args = {
     "owner": "airflow",
-    # "start_date": days_ago(1),
+    "start_date": days_ago(1),
     "depends_on_past": False,
     "retries": 1,
 }
@@ -97,52 +139,43 @@ default_args = {
 # Define DAG. This TaskGroup runs around 19 mins.
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
 with DAG(
-    dag_id="stocksdata_gcs_bq_dbt_dag",
-    # schedule_interval="@daily",
+    dag_id="newdata_gcs_bq_dbt_dag",
+    schedule_interval="@daily",
     # schedule_interval=None,
     default_args=default_args,
     catchup=False,
     max_active_runs=1,
     tags=['dtc-de'],
 ) as dag:
+   
+    extract_new_data_task = PythonOperator(
+        task_id=f"extract_new_data",
+        python_callable=extract_new_data,
+        op_kwargs={
+            "tickers": TICKERS, 
+            "last_date": last_date,                   
+        },
+    )            
 
-    extract_upload_tasks = []
-    for n in range((len(TICKERS)//50)+1): 
-        with TaskGroup(group_id=f"extract_data_{n}") as task_group1:
+    local_to_gcs_task = PythonOperator(
+        task_id=f"local_to_gcs",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/stocks_data/new_data.parquet",
+            "local_file": f"{path_to_local_home}/new_data.parquet",
+        },
+    )
 
-            extract_data_task = PythonOperator(
-                task_id=f"extract_data_{n}",
-                python_callable=extract_data,
-                op_kwargs={
-                    "tickers": TICKERS,
-                    "n": n,
-                },
-            )            
-
-            local_to_gcs_task = PythonOperator(
-                task_id=f"local_to_gcs_{n}",
-                python_callable=upload_to_gcs,
-                op_kwargs={
-                    "bucket": BUCKET,
-                    "object_name": f"raw/stocks_data/sp500_stocks_{n}.parquet",
-                    "local_file": f"{path_to_local_home}/sp500_stocks_{n}.parquet",
-                },
-            )
-
-            extract_data_task >> local_to_gcs_task
-            # upload_tasks.append(extract_data_task)
-            # upload_tasks.append(local_to_gcs_task)        
-        extract_upload_tasks.append(task_group1)
-
-    # Task to load CSVs from GCS to BigQuery
-    load_parquets_to_bq = GCSToBigQueryOperator(
-        task_id=f"load_parquets_to_bq",
+    # Task to load parquet from GCS to BigQuery staging table
+    load_parquets_to_bq_staging = GCSToBigQueryOperator(
+        task_id=f"load_parquets_to_bq_staging",
         bucket=BUCKET,
-        source_objects=["raw/stocks_data/sp500_stocks*.parquet"],
-        destination_project_dataset_table=f'{PROJECT_ID}.{BIGQUERY_DATASET}.sp500_stocks_table',
+        source_objects=["raw/stocks_data/new_data.parquet"],
+        destination_project_dataset_table=f'{PROJECT_ID}.{BIGQUERY_DATASET}.staging_data_table',
         skip_leading_rows=1,
         source_format='PARQUET',
-        write_disposition='WRITE_APPEND', # or WRITE_TRUNCATE to overwrite
+        write_disposition='WRITE_TRUNCATE', # or WRITE_TRUNCATE to overwrite
         field_delimiter=",",
         schema_fields=[
             {"name": "Date", "type": "DATE", "mode": "NULLABLE"},
@@ -156,6 +189,17 @@ with DAG(
         autodetect=False,  # Disable autodetect to enforce schema
     )
 
+    # Task to update existing records and insert new ones.
+    merge_task = PythonOperator(
+        task_id="merge_into_bigquery",
+        python_callable=merge_into_bigquery,  
+        op_kwargs={
+            "dataset_id": BIGQUERY_DATASET, 
+            "target_table": f"sp500_stocks_table", 
+            "staging_table": f"staging_data_table",
+        },      
+    )
+
     # Task to run a dbt project 
     dbt_run_task = BashOperator(
         task_id="dbt_run_task",
@@ -164,5 +208,8 @@ with DAG(
     )
 
     # Correct dependencies (Read Graph to check if dependencies are right)
-    extract_upload_tasks >> load_parquets_to_bq >> dbt_run_task
-    
+    extract_new_data_task >> local_to_gcs_task >> load_parquets_to_bq_staging >> merge_task >> dbt_run_task
+
+
+
+
